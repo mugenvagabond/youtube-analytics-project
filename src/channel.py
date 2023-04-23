6a03db9b275801b0fdcb2ba4c2f41a852997363f
Nikita Vlakhno <monkruus96@gmail.com>
2023-04-24 01:32:28 +0300

    Реализована задача инициализации экземпляра класса по id, подтягивания данных по API, вывод в консоль информации о канале в текстовом виде json формата

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,5 @@
+import json
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -5,8 +7,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key = 'AIzaSyBdmcbBlmf11PoNl0AamVO6yjw6DCeoeQ4'
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey=self.api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
